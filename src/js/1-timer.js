c3f35dccfe937a8dbb5d3670a652dbdc92a52cfd
Nataliya Yakovyna <yakovina.n13@gmail.com>
2024-06-08 00:31:36 +0300

    modified:   src/1-timer.html

---
+++ src/js/1-timer.js
@@ -0,0 +1,26 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    inputEl: document.getElementById('datetime-picker'),
+    
+};
+console.log(refs.inputEl);
+// console.log();
+// console.log();
+// console.log();
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+// function flatpickr(selector, options) {
+
+// };
+
+// flatpickr(refs.inputEl, options);
